35d676c2dafca3b18386103b97c0b6d1c0fbd3fe
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-11-04 17:29:12 +0530

    Update UserController.java

--- backend/src/main/java/com/services/backend/controller/UserController.java
+++ backend/src/main/java/com/services/backend/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.services.backend.controller;
 
+import com.services.backend.entity.Telcoservices;
+import com.services.backend.repository.TelcoservicesRepository;
 import com.services.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
@@ -14,8 +18,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    TelcoservicesRepository telcoservicesRepository;
+
+
     @GetMapping("/getAllServices")
-    public String getServices(){
-        return "This is giving all the services";
+    public List<Telcoservices> getServices(){
+
+        return telcoservicesRepository.findAll();
     }
 }
